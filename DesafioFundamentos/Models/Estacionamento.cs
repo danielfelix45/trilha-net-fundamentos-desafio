519bc1488f90e9bcabdd18d887f5f1389da18cce
Daniel Felix <felixdaniel-developer@outlook.com>
2024-05-13 18:10:24 -0300

    Melhorando o espaçamento do código no arquivo Estacionamento.cs

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -27,11 +27,13 @@ namespace DesafioFundamentos.Models
 
         public void RemoverVeiculo()
         {
+
             Console.WriteLine("Digite a placa do veículo para remover:");
             string placa = Console.ReadLine().ToUpper();
 
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
+
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
                 int horas = Convert.ToInt32(Console.ReadLine());
 
@@ -39,13 +41,14 @@ namespace DesafioFundamentos.Models
 
                 veiculos.Remove(placa);
 
-
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+
             }
             else
             {
                 Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
             }
+
         }
 
         public void ListarVeiculos()
@@ -53,12 +56,14 @@ namespace DesafioFundamentos.Models
             // Verifica se há veículos no estacionamento
             if (veiculos.Any())
             {
+
                 Console.WriteLine("Os veículos estacionados são:");
 
                 foreach (var veiculo in veiculos)
                 {
                     Console.WriteLine(veiculo);
                 }
+
             }
             else
             {
